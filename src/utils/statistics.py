d7fb88e0c23586522827fc47b3d9c58b07137816
cvigo <cvigo@baufest.com>
2022-08-09 00:43:26 -0300

    Lunes
    Add calcular frecuency de unique
    data wranglind estructuracion terminado

--- src/utils/statistics.py
+++ src/utils/statistics.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def get_upper_limit_whisker(column_df):
     """
     Calculate the max whisker value.
@@ -16,3 +19,16 @@ def get_lower_limit_whisker(column_df):
     iqr = column_df.quantile(0.75) - column_df.quantile(0.25)
     # Obtenemos el valor limite del boxplot 
     return column_df.quantile(0.25) - 1.5 * (iqr)
+
+def calculate_frequency_values(df, column):
+    """
+    Calculate the frequency of each value in a column.
+    """
+    # Vemos los valores de type
+    ps_frequency_unique = df[column].value_counts(normalize=True).map(lambda x: round(x, 3)*100).sort_values(ascending=False)
+    # accum frequency of values of ps_frequency_unique_type
+    accumulated_percentage= ps_frequency_unique.cumsum()
+    df_frecuency = pd.concat([ps_frequency_unique, accumulated_percentage], axis=1)
+    # change column names
+    df_frecuency.columns = ['porcentaje', 'porcentaje_acumulado']
+    return df_frecuency
